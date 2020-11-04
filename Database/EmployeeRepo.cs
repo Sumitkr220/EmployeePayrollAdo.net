47a179f74f8067296bdbf64cfcee73b27b82b746
sumitpandit <sumitpandit178@gmail.com>
2020-11-04 20:48:53 +0530

    [Sumit] add: Connecting database to employee payroll

---
+++ Database/EmployeeRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Database
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = "Data Source= LAPTOP-HGH2MEKE\\CAPGEMINI ;Initial Catalog=payroll_service;" + "Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeeModel employeeModel = new EmployeeModel();
+                using (this.connection)
+                {
+                    string query = @"Select * from employeePayroll;";
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+                    this.connection.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            employeeModel.EmployeeID = dr.GetInt32(0);
+                            System.Console.WriteLine(employeeModel.EmployeeID);
+                            System.Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("No data found");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Database/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeID = 10;
+            employee.EmployeeName = "Sumit";
+            employee.StartDate = DateTime.Now;
+            employee.Gender = 'M';
+            employee.PhoneNumber = "9012872727";
+            employee.Department = "Tech";
+            employee.City = "Kolkata";
+            employee.BasicPay = 10000;
+            employee.EmployeeID = 101;
+            employee.Address = "8/1";
+            employee.Deductions = 500;
+            employee.TaxablePay = 200;
+            employee.Tax = 899;
+            employee.NetPay = 200000;
+            employee.Country = 70002;
+
+            repo.GetAllEmployee();
+        }
+    }
+}
