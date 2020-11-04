16722dc7483ebd18767c243a9316c79ac017eac7
sumitpandit <sumitpandit178@gmail.com>
2020-11-04 20:58:45 +0530

    [Sumit] add: Retrieving employee payroll from database

--- Database/EmployeeRepo.cs
+++ Database/EmployeeRepo.cs
@@ -25,7 +25,18 @@ namespace Database
                         while (dr.Read())
                         {
                             employeeModel.EmployeeID = dr.GetInt32(0);
-                            System.Console.WriteLine(employeeModel.EmployeeID);
+                            employeeModel.EmployeeName = dr.GetString(1);
+                            employeeModel.BasicPay = dr.GetDecimal(2);
+                            employeeModel.StartDate = dr.GetDateTime(3);
+                            employeeModel.Gender = Convert.ToChar(dr.GetString(4));
+                            employeeModel.PhoneNumber = dr.GetString(5);
+                            employeeModel.Address = dr.GetString(6);
+                            employeeModel.Department = dr.GetString(7);
+                            employeeModel.Deductions = dr.GetDouble(8);
+                            employeeModel.TaxablePay = (double)dr.GetSqlSingle(9);
+                            employeeModel.Tax = dr.GetDouble(10);
+                            employeeModel.NetPay = (double)dr.GetSqlSingle(11);
+                            System.Console.WriteLine(employeeModel.EmployeeName + "   " + employeeModel.BasicPay + "   " + employeeModel.StartDate + "   " + employeeModel.Gender + "   " + employeeModel.PhoneNumber + "   " + employeeModel.Address + "   " + employeeModel.Department + "   " + employeeModel.Deductions + "   " + employeeModel.TaxablePay + "   " + employeeModel.Tax + "   " + employeeModel.NetPay);
                             System.Console.WriteLine("\n");
                         }
                     }
